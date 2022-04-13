34e09bed5b20359d3aa10a4b63ea386b00b067e3
Franco D. Ocaranza <netfrank777@gmail.com>
2022-04-13 10:49:26 -0300

    Ignore secret.js

--- secret.ts
+++ secret.ts
@@ -1,6 +1,6 @@
 export const Secret: any = {
     api: {
         url: 'http://api.kbzas.ar/public/',
-        token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzaXRlIjoidmlsbGFyIiwiaWQiOjEwfQ.gdBfDAI11H5ZOx2_Z_-PU9aLYW5LCj76Hw-V-19ESOQ'
+        token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzaXRlIjoidmlsbGFyIiwiaWQiOjEwfQ.gdBfDAI11H5ZOx2_Z_-PU9aLYW5LCj76Hw-V-19ESOQ',
     }
 };
